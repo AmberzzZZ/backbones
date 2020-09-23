e47e17df8fe7320d2591c8f61c420feef1ee9933
amber.zhang <774524217@qq.com>
2020-09-23 19:40:56 +0800

    effb0_b7

--- googleNet/efficientNet.py
+++ googleNet/efficientNet.py
@@ -2,16 +2,24 @@ from keras.layers import Input, Conv2D, BatchNormalization, Activation, Depthwis
                          GlobalAveragePooling2D, Reshape, Dropout, add, Dense
 from keras.models import Model
 import keras.backend as K
+import math
 
 
 DEFAULT_BLOCKS_ARGS = [
-    {'kernel_size': 3, 'repeats': 1, 'filters_in': 32,  'filters_out': 16,  'expand_ratio': 1, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25},
-    {'kernel_size': 3, 'repeats': 2, 'filters_in': 16,  'filters_out': 24,  'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
-    {'kernel_size': 5, 'repeats': 2, 'filters_in': 24,  'filters_out': 40,  'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
-    {'kernel_size': 3, 'repeats': 3, 'filters_in': 40,  'filters_out': 80,  'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
-    {'kernel_size': 5, 'repeats': 3, 'filters_in': 80,  'filters_out': 112, 'expand_ratio': 6, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25},
-    {'kernel_size': 5, 'repeats': 4, 'filters_in': 112, 'filters_out': 192, 'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
-    {'kernel_size': 3, 'repeats': 1, 'filters_in': 192, 'filters_out': 320, 'expand_ratio': 6, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25}
+    {'kernel_size': 3, 'repeats': 1, 'filters_in': 32, 'filters_out': 16,
+     'expand_ratio': 1, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25},
+    {'kernel_size': 3, 'repeats': 2, 'filters_in': 16, 'filters_out': 24,
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
+    {'kernel_size': 5, 'repeats': 2, 'filters_in': 24, 'filters_out': 40,
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
+    {'kernel_size': 3, 'repeats': 3, 'filters_in': 40, 'filters_out': 80,
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
+    {'kernel_size': 5, 'repeats': 3, 'filters_in': 80, 'filters_out': 112,
+     'expand_ratio': 6, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25},
+    {'kernel_size': 5, 'repeats': 4, 'filters_in': 112, 'filters_out': 192,
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
+    {'kernel_size': 3, 'repeats': 1, 'filters_in': 192, 'filters_out': 320,
+     'expand_ratio': 6, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25}
 ]
 
 
@@ -38,20 +46,27 @@ DENSE_KERNEL_INITIALIZER = {
 }
 
 
-def EfficientNet(input_tensor=None, input_shape=(224,224,3), n_classes=1000,
-                 drop_connect_rate=0., dropout_rate=0.):
-    if input_tensor is not None:
-        inpt = input_tensor
-    else:
+def EfficientNet(input_shape, width_coefficient, depth_coefficient, dropout_rate=0.2,
+                 n_classes=1000, drop_connect_rate=0.2, blocks_args=DEFAULT_BLOCKS_ARGS,
+                 depth_divisor=8):
+
+    if isinstance(input_shape, tuple):
         inpt = Input(input_shape)
+    else:
+        inpt = Input((input_shape,input_shape,3))
 
     # stem
-    x = Conv_BN(inpt, 32, 3, strides=2)
+    x = Conv_BN(inpt, round_filters(32, width_coefficient), 3, strides=2)
 
     # blocks
     b = 0
     blocks = float(sum(args['repeats'] for args in DEFAULT_BLOCKS_ARGS))
+    # for each block group
     for block_arg in DEFAULT_BLOCKS_ARGS:
+        # for each mbconv block
+        block_arg['repeats'] = round_repeats(block_arg['repeats'], depth_coefficient)
+        block_arg['filters_in'] = round_filters(block_arg['filters_in'], width_coefficient)
+        block_arg['filters_out'] = round_filters(block_arg['filters_out'], width_coefficient)
         for i in range(block_arg['repeats']):
             if i > 0:
                 block_arg['filters_in'] = block_arg['filters_out']
@@ -59,9 +74,8 @@ def EfficientNet(input_tensor=None, input_shape=(224,224,3), n_classes=1000,
             x = efficientBlock(x, drop_connect_rate*b/blocks, **block_arg)
             b += 1
 
-    x = Conv_BN(x, 1280, 1, strides=1)
-
     # top
+    x = Conv_BN(x, round_filters(1280, width_coefficient), 1, strides=1)
     x = GlobalAveragePooling2D()(x)
     if dropout_rate > 0:
         x = Dropout(dropout_rate)(x)
@@ -72,7 +86,8 @@ def EfficientNet(input_tensor=None, input_shape=(224,224,3), n_classes=1000,
     return model
 
 
-def efficientBlock(x, drop_rate=0., kernel_size=3, repeats=1, filters_in=32, filters_out=16, expand_ratio=1, id_skip=True, strides=1, se_ratio=0.25):
+def efficientBlock(x, drop_rate=0., kernel_size=3, repeats=1, filters_in=32, filters_out=16,
+                   expand_ratio=1, id_skip=True, strides=1, se_ratio=0.25):
     inpt = x
 
     # PW-expand
@@ -81,11 +96,12 @@ def efficientBlock(x, drop_rate=0., kernel_size=3, repeats=1, filters_in=32, fil
         x = Conv_BN(x, n_filters, kernel_size=1, strides=1)
 
     # DW conv
-    x = DW_Conv_BN(x, kernel_size=3, strides=strides)
+    x = DW_Conv_BN(x, kernel_size=kernel_size, strides=strides)
 
     # SE-block
     if se_ratio:
-        x = se_block(x, int(filters_in*se_ratio))
+        filters_se = max(1, int(filters_in*se_ratio))
+        x = se_block(x, filters_se)
 
     # PW-project
     x = Conv_BN(x, filters_out, kernel_size=1, strides=1, activation=None)
@@ -120,26 +136,75 @@ def swish(x):
     return x * K.sigmoid(x)
 
 
-def Conv_BN(x, n_filters, kernel_size=3, strides=1, kernel_initializer=CONV_KERNEL_INITIALIZER, activation=swish):
-    x = Conv2D(n_filters, kernel_size, strides=strides, padding='same', kernel_initializer=kernel_initializer)(x)
+def Conv_BN(x, n_filters, kernel_size=3, strides=1, padding='same', activation=swish,
+            kernel_initializer=CONV_KERNEL_INITIALIZER):
+    x = Conv2D(n_filters, kernel_size, strides=strides, padding=padding, use_bias=False, kernel_initializer=kernel_initializer)(x)
     x = BatchNormalization()(x)
     if activation:
         x = Activation(activation)(x)
     return x
 
 
-def DW_Conv_BN(x, kernel_size=3, strides=1, kernel_initializer=CONV_KERNEL_INITIALIZER, activation=swish):
-    x = DepthwiseConv2D(kernel_size, strides=strides, padding='same', depth_multiplier=1, depthwise_initializer=kernel_initializer)(x)
+def DW_Conv_BN(x, kernel_size=3, strides=1, padding='same', activation=swish,
+               kernel_initializer=CONV_KERNEL_INITIALIZER):
+    x = DepthwiseConv2D(kernel_size, strides=strides, padding=padding, use_bias=False, depthwise_initializer=kernel_initializer)(x)
     x = BatchNormalization()(x)
     if activation:
         x = Activation(activation)(x)
     return x
+
+
+def round_filters(filters, width_coefficient, divisor=8):
+    filters *= width_coefficient
+    new_filters = max(divisor, int(filters + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_filters < 0.9 * filters:
+        new_filters += divisor
+    return int(new_filters)
+
+
+def round_repeats(repeats, depth_coefficient):
+    return int(math.ceil(depth_coefficient * repeats))
+
+
+########### effNets family ########
+def EfficientNetB0():
+    return EfficientNet(224, 1.0, 1.0, 0.2)
+
+
+def EfficientNetB1():
+    return EfficientNet(240, 1.0, 1.1, 0.2)
+
+
+def EfficientNetB2():
+    return EfficientNet(260, 1.1, 1.2, 0.3)
+
+
+def EfficientNetB3():
+    return EfficientNet(300, 1.2, 1.4, 0.3)
+
+
+def EfficientNetB4():
+    return EfficientNet(380, 1.4, 1.8, 0.4)
+
+
+def EfficientNetB5():
+    return EfficientNet(456, 1.6, 2.2, 0.4)
+
+
+def EfficientNetB6():
+    return EfficientNet(528, 1.8, 2.6, 0.5)
+
+
+def EfficientNetB7():
+    return EfficientNet(600, 2.0, 3.1, 0.5)
 
 
 if __name__ == '__main__':
 
-    model = EfficientNet(input_shape=(224,224,3), n_classes=1000)
-    model.summary()
+    model = EfficientNetB4()
+    model.load_weights("/Users/amber/Downloads/Misc/efficientnet-b4_weights_tf_dim_ordering_tf_kernels.h5", by_name=True, skip_mismatch=True)
+    # model.summary()
 
 
 
--- googleNet/readme.md
+++ googleNet/readme.md
@@ -3,3 +3,67 @@
     2. 所有的maxpooling替换成了stride2的separable conv
     3. 所有的depthwise conv后面都有BN+ReLU，和mobileNet里是一样的，DW-BN-ReLU-PW-BN-ReLU
        原始的SeparableConv2D则是[DW-PW]-BN-ReLU
+
+
+
+## efficientNet B0-B7
+    1. 从论文的acc pk上看，B0的精度就超过resnet了，B3及以上吊打resdeeper家族
+    2. 但是我用B0实际实验（简单六分类）下来并没有
+    3. B0-B7: finer & wider & deeper, input_size增大，channel加宽，layers加深
+        |  network  | input_size |   width  |  deepth  | dropout_rate |
+        |     B0    |     224    |    1.0   |    1.0   |      0.2     |
+        |     B1    |     240    |    1.0   |    1.1   |      0.2     | 
+        |     B2    |     260    |    1.1   |    1.2   |      0.3     | 
+        |     B3    |     300    |    1.2   |    1.4   |      0.3     |
+        |     B4    |     380    |    1.4   |    1.8   |      0.4     |
+        |     B5    |     456    |    1.6   |    2.2   |      0.4     | 
+        |     B6    |     528    |    1.8   |    2.6   |      0.5     |
+        |     B7    |     600    |    2.0   |    3.1   |      0.5     |
+    4. pip库: https://github.com/qubvel/efficientnet
+        Requirements: 
+        Keras >= 2.2.0 / TensorFlow >= 1.12.0
+        keras_applications >= 1.0.7
+        scikit-image
+
+    5. todo：B4(https://www.kaggle.com/jimitshah777/bilinear-efficientnet-focal-loss-label-smoothing)
+
+
+## efficientNet details
+    1. input 
+        input shape: >=32
+        input value: imagenet_utils.preprocess_input输出[-1,1]
+
+    2. scale
+        round_filters: 基于width_coefficient加宽
+        depth_divisor=8, 确保网络宽度是8的倍数
+        round_repeats: 基于depth_coefficient加深
+
+    3. DEFAULT_BLOCKS_ARGS
+        kernel size 和 strides的值是针对depthwise conv的
+        其余block都有固定的kernel size
+
+    4. drop_connect_rate
+        用在mbconv block里面，随着层数加深这个值逐渐增大
+        dropout: 将输入单元的按比率随机设置为0，二维特征有一个noisy_shape参数
+        rate: 是drop rate，不是keep prob
+        noisy_shape: 
+            是一个一维张量，和输入x的shape的长度一样，e.g. x_shape: [B,H,W,C] ---> noisy_shape: [?,?,?,?]
+            其中的元素只能是1或者x的shape中对应元素，哪个轴为1，哪个轴就会被一致地dropout
+            noise_shape=(None, 1, 1, 1)意味着我们希望一个batch的数据都遵循同一种的dropout模式————每个hwc的样本上被mute掉的元素位置相同
+
+    5. residual condition
+        if id_skip is True and strides==1 and filters_in==filters_out:
+
+    6. dropout
+        用在分类头上，
+
+    7. activation
+        全程swish
+        swish最早出现在MobileNetV3，effNet用的block就是MobileNetV3的block，
+        但是MobileNetV3不是只在深层block里面用了swish，因为论文里说实验下来这样好
+
+
+
+
+
+
